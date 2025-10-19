cfb1ec84e974e9644302ee96326d255b744a307d
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-10-19 07:38:54 -0700

    Assigning appropriate dtypes on initial dataframe load

--- cfclone/run/postprocess.py
+++ cfclone/run/postprocess.py
@@ -123,7 +123,11 @@ def _load_df(fh, key):
 
     cols = fh[key].attrs["columns"]
 
-    return pd.DataFrame(vals, columns=cols)
+    df = pd.DataFrame(vals, columns=cols)
+
+    df = df.convert_dtypes()
+
+    return df
 
 
 def _load_rho(data, samples_df, normal=False, renormalise=True):
