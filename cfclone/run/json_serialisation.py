cab6f691d8f42f337a220568415839d65db78381
Andrew Roth <aroth@bccrc.ca>
2025-12-16 15:00:14 -0800

    black formatting

--- cfclone/run/json_serialisation.py
+++ cfclone/run/json_serialisation.py
@@ -23,17 +23,17 @@ def serialise_networkx_tree_to_json(nx_tree, json_file):
 
     tree_data = nx.node_link_data(nx_tree, edges="edges")
 
-    with open(json_file, 'w') as f:
+    with open(json_file, "w") as f:
         json.dump(tree_data, f, indent=4, cls=NumpyPandasEncoder)
 
 
 def serialise_dict_to_json(python_dict, json_file):
 
-    with open(json_file, 'w') as f:
+    with open(json_file, "w") as f:
         json.dump(python_dict, f, indent=4, cls=NumpyPandasEncoder)
 
 
 def read_json_into_dict(json_file):
-    with open(json_file, 'r') as f:
+    with open(json_file, "r") as f:
         res_dict = json.load(f)
     return res_dict
